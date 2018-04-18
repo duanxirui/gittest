c79f7383b263e236c31307dbeb7584d6cd94766b
duanxirui <846083855@qq.com>
2018-04-18 13:44:43 +0800

    段锡瑞 添加index.php文件

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-04-18 13:43:24
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+    
+    function __construct(){
+        echo 'skdhfkj';
+    }
+}
